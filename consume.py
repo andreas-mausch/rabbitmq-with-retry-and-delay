8d0466b5c780ea9a518f129a4665cf383ac5d7d7
neonew <neonew@gmail.com>
2024-08-14 22:51:37 +0200

    Set a maximum retry count

--- consume.py
+++ consume.py
@@ -26,9 +26,25 @@ def my_callback(channel, method, properties, body):
 
 def error_callback(channel, method, properties, body):
     logger.info(f"<< error-queue: {method.routing_key}:{body}")
+    logger.info(f"                reason: {properties.headers['x-first-death-reason']}")
+    logger.info(f"                exchange: {properties.headers['x-first-death-exchange']} / queue: {properties.headers['x-first-death-queue']}")
+    logger.info(f"                {properties.headers['x-death']}")
+
+    reject_count = next((death for death in properties.headers['x-death'] if death['reason'] == 'rejected'))['count']
+
+    if reject_count < 0 or reject_count >= 5:
+        logger.info(f"XX re-queue limit exceeeded, too many rejections: {reject_count}. Dropping message.")
+        channel.basic_reject(method.delivery_tag, requeue=False)
+        return
+
+    channel.basic_publish(exchange=properties.headers['x-first-death-exchange'],
+                          routing_key=properties.headers['x-death'][0]['routing-keys'][0],
+                          body=body,
+                          properties=properties)
+    channel.basic_ack(method.delivery_tag)
 
 channel.basic_consume(queue='my-queue', on_message_callback=my_callback, auto_ack=False)
-channel.basic_consume(queue='error-queue', on_message_callback=error_callback, auto_ack=True)
+channel.basic_consume(queue='error-queue', on_message_callback=error_callback, auto_ack=False)
 
 try:
     channel.start_consuming()
